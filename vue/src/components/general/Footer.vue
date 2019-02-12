d4b8b82253140fe9019f8622f9bb0cf5d69f25c0
Alef Barbeli <alefbarbeli@gmail.com>
2019-02-12 20:13:48 -0200

    vue version update

---
+++ vue/src/components/general/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+    <footer>
+        <div class="soc">
+            <a target="_blank" href="https://github.com/"><span class="ion ion-social-github"></span></a>
+            <a target="_blank" href="https://www.instagram.com/"><span class="ion ion-social-linkedin"></span></a>
+        </div>
+        <div class="copy">© 2016 Glitche.</div>
+        <div class="clr"></div>
+    </footer>
+</template>
---
+++ vue/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="section started">
+    <div class="centrize full-width">
+      <div class="vertical-center">
+        <div class="started-content">
+          <div class="h-title glitch-effect" data-text="Alef Barbeli"><span>Alef Barbeli</span></div>
+          <div class="h-subtitle typing-subtitle">
+            <p>São Paulo/ SP - Brasil</p>
+            <p>Desenvolvedor Front-End</p>
+            <p>Especialista e-commerce</p>
+            <p>Vue.js, Angular, React</p>
+            <p>HTML5, CSS3, ES6, API Rest</p>
+          </div>
+          <span class="typed-subtitle"></span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
