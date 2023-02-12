e21b3ee252402896ec06c3c16d566b4ae6d54318
Alef Barbeli <alef.barbeli@digitalboutique.co.uk>
2023-02-11 23:23:32 -0300

    migrate PreRender to Nuxt Content

---
+++ components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+    <footer>
+        <div class="soc">
+            <a target="_blank" href="https://github.com/"><span class="ion ion-social-github"></span></a>
+            <a target="_blank" href="https://www.instagram.com/"><span class="ion ion-social-linkedin"></span></a>
+        </div>
+        <!-- <div class="copy">© 2023 Alef Barbeli.</div> -->
+        <div class="clr"></div>
+    </footer>
+</template>
