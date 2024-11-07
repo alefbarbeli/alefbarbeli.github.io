48d8c486ece06e1182e8b8dbfe458ad0b78fe823
Alef Barbeli <alefbarbeli@gmail.com>
2024-11-07 19:17:13 -0300

    refactor: performance 100

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content']
+  modules: [
+    '@nuxt/content',
+    '@nuxtjs/google-fonts'
+  ],
+  googleFonts: {
+    families: {
+      'Share Tech Mono': true
+    }
+  },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'pt-BR'
+      }
+    }
+  }
 })
