c68e89a4567eb4e3703b103e6688da9d747d0633
Alef Barbeli <alefbarbeli@gmail.com>
2024-11-07 18:53:23 -0300

    refactor: improve SEO

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
 
     <!-- Container -->
     <div class="container">
-      
+
       <!-- Wrapper -->
       <div class="wrapper">
 
@@ -12,12 +12,25 @@
           <NuxtPage />
         <!-- </transition> -->
         <Footer />
-      
+
       </div>
-      
+
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Alef Barbeli, Front-end Developer',
+  ogTitle: 'Alef Barbeli, Front-end Developer',
+  description: 'Portfolio and Resume of Alef Barbeli, Front-end Developer',
+  ogDescription: 'Portfolio and Resume of Alef Barbeli, Front-end Developer',
+  ogImage: '/profile.jpg',
+  twitterCard: 'summary_large_image',
+  charset: 'utf-8',
+  viewport: 'width=device-width, initial-scale=1',
+})
+</script>
 
 <style lang="scss">
 @import 'assets/scss/animate.scss';
