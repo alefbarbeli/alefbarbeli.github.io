24bbfd3c6b9540240c9fa1c30a7094691b8a9755
Alef Barbeli <alefbarbeli@gmail.com>
2024-11-12 13:27:13 -0300

    feat: performance improvements

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,17 +3,29 @@ export default defineNuxtConfig({
     '@nuxt/content',
     '@nuxtjs/google-fonts'
   ],
+
   googleFonts: {
     families: {
       'Share Tech Mono': true
     },
     outputDir: 'assets'
   },
+
   app: {
     head: {
       htmlAttrs: {
         lang: 'pt-BR'
       }
     }
-  }
+  },
+
+  css: [
+    'assets/scss/animate.scss',
+    'assets/scss/glitche-basic.scss',
+    'assets/scss/glitche-layout.scss',
+    // 'assets/scss/ionicons.scss',
+    'assets/scss/magnific-popup.scss'
+  ],
+
+  compatibilityDate: '2024-11-11'
 })
